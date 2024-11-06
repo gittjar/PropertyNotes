8c98ae0ac85456bae58057073fb50583dec33363
gittjar <jarno.keranen@gmail.com>
2024-11-06 19:57:20 +0200

    server.js test

---
+++ backend/server2.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require('./routes'); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connection established'))
+  .catch(err => console.log('MongoDB connection error: ', err));
+
+// Use routes
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
